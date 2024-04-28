7d693043877fefd9f915439d4035f13250eb1daf
Roy <roy@osherove.com>
2024-04-28 15:08:43 +0300

    build for sepolia base

--- examples/simple/hardhat.config.ts
+++ examples/simple/hardhat.config.ts
@@ -2,8 +2,19 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 import {deplooy} from "contractoor"
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+     // for testnet
+     'base-sepolia': {
+      url: 'https://sepolia.base.org',
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    } 
+  },
 };
 
 task("deploy", "Deploys contracts based on the configuration", async (_, hre) => {
